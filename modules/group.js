5374eba51eba5584781113fcc488a44e31f3967c
jeankoudjoougoum-debug <jeankoudjoougoum@gmail.com>
2026-08-10 11:37:57 +0000

    Create modules/group.js

---
+++ modules/group.js
@@ -0,0 +1,9 @@
+// Tag all
+async function tagAll(conn, msg, sender) {
+    // Mentionne tout le monde dans le groupe
+}
+
+// Set welcome message
+async function welcomeToggle(conn, msg, args, sender) {
+    // Active/désactive le message de bienvenue
+}
