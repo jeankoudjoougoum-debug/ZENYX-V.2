e4497528f4226d9a54ef2b94785bb076d2a3771f
jeankoudjoougoum-debug <jeankoudjoougoum@gmail.com>
2026-08-10 11:38:37 +0000

    Create modules/admin.js

---
+++ modules/admin.js
@@ -0,0 +1,14 @@
+// Broadcast
+async function broadcast(conn, msg, args, sender) {
+    // Envoie un message à tous les groupes / utilisateurs
+}
+
+// Stats
+async function statsBot(conn, msg, sender) {
+    // Affiche les stats du bot (messages, commandes, uptime)
+}
+
+// Actif
+async function actif(conn, msg, sender) {
+    // Affiche les membres les plus actifs avec leurs messages
+}
