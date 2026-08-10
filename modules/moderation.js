1e8f36ad9b8e53971ccf9eba8135a927644079e4
jeankoudjoougoum-debug <jeankoudjoougoum@gmail.com>
2026-08-10 11:37:25 +0000

    Create modules/moderation.js

---
+++ modules/moderation.js
@@ -0,0 +1,14 @@
+// Gestion des warns
+async function warnUser(conn, msg, args, sender) {
+    // Ajoute un warn à l'utilisateur
+}
+
+// Mute temporaire
+async function muteUser(conn, msg, args, sender) {
+    // Mute un utilisateur pour X minutes
+}
+
+// Ban du bot
+async function banUser(conn, msg, args, sender) {
+    // Bannir un utilisateur du bot
+}
